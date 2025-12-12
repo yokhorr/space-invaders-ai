13bc2a04962c803293b946f85b69782c3eb54ed0
Egor Solyanik <yokhor06@mail.ru>
2025-12-12 10:56:14 +0300

    chore: fully remove dist/ from git index Update .gitignore to include dist/. Remove all build artifacts from version control. This ensures dist/ is never tracked again.

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const game_1 = require("./ui/game");
-window.onload = () => {
-  const canvas = document.getElementById("game");
-  new game_1.Game(canvas);
-};
